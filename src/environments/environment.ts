d126ddb5f8a32dc6846631b57a568361d4de6f92
Mert Kaya <98533679+Mkyaa@users.noreply.github.com>
2024-05-03 11:56:23 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,1 @@
-export const environment = {
-    production: true,
-    apiURL: 'https://logointegrationdemo.iesyazilim.com.tr/api/',
-    tokenURL: 'https://logointegrationdemo.iesyazilim.com.tr/connect/token'
-};
+
